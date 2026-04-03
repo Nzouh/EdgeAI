8ca974685f144fd8e3e072121b0a2075279a7b2e
Nzouh <nzouh055@uottawa.ca>
2026-04-03 17:28:19 -0400

    Adding quantization logic

---
+++ quantize/lab.py
@@ -0,0 +1,58 @@
+from torch.ao.quantization.fx._decomposed import dequantize_per_tensor
+import torch
+
+def get_symmetric_scale(x):
+
+    """
+    Finding the scale factor
+    Ex: If weights range from -2 to 2, scale is 2/127 = 0.0157
+    """
+
+    max_val = torch.max(torch.abs(x))
+    scale = max_val / 127
+    return scale
+
+def quantize_symmetric(x, scale):
+    """
+    Converting from Float32 -> Int8
+    """
+
+    quantized = torch.round(x / scale)
+
+    quantized = torch.clamp(quantized, -128, 127)
+    return quantized.to(torch.int8)
+
+def dequantize_symmetric(x, scale):
+    """
+    Converting from Int8 -> Float32
+    """
+
+    return x.to(torch.float32) * scale
+
+
+
+if __name__ == "__main__":
+    #Create a random Weight matrix (to represent MLP)
+    torch.manual_seed(42)
+    weights = torch.randn(4, 4)
+    print(f"Original Weights : {weights}")
+
+    scale = get_symmetric_scale(weights)
+
+    print(f"\nscale: {scale.item():.7f}")
+
+    quantized_weight = quantize_symmetric(weights, scale)
+
+    print(f"\nQuantized Weights: {quantized_weight}")
+
+    dequantized_weight = dequantize_symmetric(quantized_weight, scale)
+    print(f"Dequantized Weights: {dequantized_weight}")
+
+
+    #Measuring Quantization Error
+    error = torch.mean((weights - dequantized_weight)**2)
+    print(f"MSE: {error.item():.8f}")
+
+
+
+    def get_asymmetric_mapping(x):
